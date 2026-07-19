dc5743b2927f3dc829e6d10afb983473be3b0f64
Yas <yasifa85@gmail.com>
2026-07-19 17:56:04 +0300

    Set up game data structures

--- rps.cpp
+++ rps.cpp
@@ -1,1 +1,27 @@
-"" > rps.cpp
+#include <iostream>
+using namespace std;
+
+enum enChoice {Rock = 1, Paper = 2, Scissors = 3};
+enum enWinner {Player1 = 1, Computer = 2, Draw = 3};
+
+struct stRound
+{
+    short roundNumber = 0;
+    enChoice playerChoice;
+    enChoice computerChoice;
+    enWinner roundWinner;
+};
+
+struct stGameResult
+{
+    short totalRounds = 0;
+    int playerWins = 0;
+    int computerWins = 0;
+    int draws = 0;
+    enWinner finalWinner;
+};
+
+int main() 
+{
+    return 0;
+}
