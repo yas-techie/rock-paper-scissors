f662d97f7018a511392245239170089e15944e29
Yas <yasifa85@gmail.com>
2026-07-19 18:05:51 +0300

    Add player and computer choice selection

--- rps.cpp
+++ rps.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 enum enChoice {Rock = 1, Paper = 2, Scissors = 3};
@@ -20,6 +22,33 @@ struct stGameResult
     int draws = 0;
     enWinner finalWinner;
 };
+
+int readNumber(string message)
+{
+    int num;
+    cout<<message;
+    cin>>num;
+    return num;
+}
+
+int randomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+enChoice getPlayerChoice()
+{
+    int choice;
+    do
+    {
+        choice = readNumber("Your Choice: [1] Rock, [2] Paper, [3] Scissors? ");
+    } while (choice < 1 || choice > 3);
+    return (enChoice)choice;
+}
+enChoice getComputerChoice()
+{
+    return (enChoice)randomNumber(1, 3);
+}
 
 int main() 
 {
