0065ea1316bd3cf5b91e6fa458f5528b61f3ee96
Yas <yasifa85@gmail.com>
2026-07-19 23:23:18 +0300

    Add round logic and winner display

--- rps.cpp
+++ rps.cpp
@@ -50,6 +50,72 @@ enChoice getComputerChoice()
     return (enChoice)randomNumber(1, 3);
 }
 
+enWinner roundWinner(stRound r)
+{
+    if (r.playerChoice == r.computerChoice)
+        return enWinner::Draw;
+
+    switch (r.playerChoice)
+    {
+    case enChoice::Rock:
+        return (r.computerChoice == enChoice::Scissors)? enWinner::Player1 : enWinner::Computer;
+
+    case enChoice::Paper:
+        return (r.computerChoice == enChoice::Rock)? enWinner::Player1 : enWinner::Computer;
+
+    case enChoice::Scissors:
+        return (r.computerChoice == enChoice::Paper)? enWinner::Player1 : enWinner::Computer;
+    
+    default:
+        return enWinner::Draw;
+    }
+    
+}
+
+string choiceName(enChoice choice)
+{
+    string choiceNames[3] = {"Rock", "Paper", "Scissors"};
+    return choiceNames[choice - 1];
+}
+string winnerName(enWinner winner)
+{
+    string winnerNames[3] = { "Player1", "Computer", "No Winner (Draw)" };
+    return winnerNames[winner - 1];
+}
+
+stRound playRound(int roundNumber)
+{
+    stRound round;
+    round.roundNumber = roundNumber;
+    round.playerChoice = getPlayerChoice();
+    round.computerChoice = getComputerChoice();
+    round.roundWinner = roundWinner(round);
+    return round;
+};
+
+int readRound()
+{
+    int num;
+    do
+    {
+        num = readNumber("How Many Rounds [1 - 10]? ");
+    } while (num < 1 || num > 10);
+    cout<<endl;
+
+    return num; 
+}
+
+void printRound(stRound round)
+{
+    cout << "\n__________ Round [" << round.roundNumber << "] _________\n" << endl;
+    
+    cout << "Player1 Choice: " << choiceName(round.playerChoice) << endl;
+    cout << "Computer Choice: " << choiceName(round.computerChoice) << endl;
+    cout << "Round Winner: " << winnerName(round.roundWinner) << endl;
+
+    cout << "______________________________\n" << endl;
+}
+
 int main() 
 {
     return 0;
